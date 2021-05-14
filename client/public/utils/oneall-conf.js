96366079ff5988543460c1c2142f925f101ef6f1
Jordan Laor <jordanlaor@gmail.com>
2021-05-14 17:09:19 +0300

    callback auth path fixed

--- client/public/utils/oneall-conf.js
+++ client/public/utils/oneall-conf.js
@@ -9,8 +9,8 @@ var _oneall = _oneall || [];
 _oneall.push([
   "social_login",
   "set_callback_uri",
-  `http://localhost:9876/api/callback?redirect=${window.location.origin}&page=${window.location.pathname}`,
-  // `https://final-project-gift-registry.herokuapp.com/api/callback?redirect=${window.location.origin}&page=${window.location.pathname}`,
+  // `http://localhost:9876/api/callback?redirect=${window.location.origin}&page=${window.location.pathname}`,
+  `https://final-project-gift-registry.herokuapp.com/api/callback?redirect=${window.location.origin}&page=${window.location.pathname}`,
 ]);
 _oneall.push(["social_login", "set_providers", ["facebook", "google"]]);
 _oneall.push(["social_login", "set_custom_css_uri", "https://secure.oneallcdn.com/css/api/themes/flat_w32_h32_wc_v1.css"]);
