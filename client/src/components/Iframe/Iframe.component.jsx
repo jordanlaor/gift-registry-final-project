942d9b873f512f8ad0f134a995f9a227a34c9086
Jordan Laor <jordanlaor@gmail.com>
2021-05-23 17:22:26 +0300

    cleaned comments

--- client/src/components/Iframe/Iframe.component.jsx
+++ client/src/components/Iframe/Iframe.component.jsx
@@ -3,9 +3,7 @@ import "./iframe.css";
 
 const Iframe = (props) => {
   const { src, onError } = props;
-  // return <iframe src="https://www.amazon.com/" title="amazon" target="_parent" is="x-frame-bypass"></iframe>;
   return <iframe id="gift-list-iframe" src={src} title={src} target="_parent" is="x-frame-bypass"></iframe>;
-  // return <iframe onError={onError} id="gift-list-iframe" src={src} title={src} target="_parent" is="x-frame-bypass"></iframe>;
 };
 
 export default Iframe;
